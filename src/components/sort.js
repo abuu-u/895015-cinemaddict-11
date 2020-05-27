45c59a2a1e95f812b758811e34a61c064b29993b
abuu-u <mr.abu13@mail.ru>
2020-05-28 01:06:48 +0500

    исправил

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,18 +1,30 @@
 import AbstractComponent from "./abstract-component";
 
 const SortType = {
+  DEFAULT: `default`,
   BY_DATE: `by-date`,
   BY_RATING: `by-rating`,
-  DEFAULT: `default`,
 };
 
+const ACTIVE_CLASS = `sort__button--active`;
+
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currenSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
+    const defaultActiveClass = this._currenSortType === SortType.DEFAULT ? ACTIVE_CLASS : ``;
+    const byDatetActiveClass = this._currenSortType === SortType.BY_DATE ? ACTIVE_CLASS : ``;
+    const byRatingActiveClass = this._currenSortType === SortType.BY_RATING ? ACTIVE_CLASS : ``;
+
     return (
       `<ul class="sort">
-      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" data-sort-type="${SortType.BY_DATE}" class="sort__button">Sort by date</a></li>
-      <li><a href="#" data-sort-type="${SortType.BY_RATING}" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button ${defaultActiveClass}">Sort by default</a></li>
+      <li><a href="#" data-sort-type="${SortType.BY_DATE}" class="sort__button ${byDatetActiveClass}">Sort by date</a></li>
+      <li><a href="#" data-sort-type="${SortType.BY_RATING}" class="sort__button ${byRatingActiveClass}">Sort by rating</a></li>
     </ul>`
     );
   }
@@ -35,7 +47,6 @@ export default class Sort extends AbstractComponent {
 
       handler(this._currenSortType);
     });
-
   }
 }
 
